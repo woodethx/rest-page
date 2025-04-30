c41e388f9350c7b116927d28cafae55e7a6fa9e5
Ethan Wood <woodethx@gmail.com>
2025-04-29 20:19:31 -0500

    Add - Contact page

--- src/contact.js
+++ src/contact.js
@@ -7,4 +7,8 @@ export default function renderContact(rootSelector) {
         button.classList.remove("active");
     })
     contactButton.classList.add("active");
+    const contactDiv = document.createElement("div");
+    contactDiv.classList.add("contact");
+    contactDiv.innerHTML = "<h3>Contact Info</h3> <p>Phone Number: (123) 456-7890</p> <p>Email: real.email@realwebsite.com<p/> <p>Address: 123 Anystreet</p>"
+    root.append(contactDiv);
 }
